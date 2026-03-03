cf596db4128920ef51d7c321bd45a10aa49e59a2
Will Senn <will.senn@gmail.com>
2026-03-02 20:35:57 -0600

    removed extraneous embed

--- extra/samples/engine/position.go
+++
@@ -1,9 +0,0 @@
-// Package engine provides core types shared across engine components.
-package engine
-
-// Position represents a source location.
-type Position struct {
-	File string
-	Line int
-	Col  int
-}
