0a253b6140dea29043a2e689c966a42f73a14e8d
Will Senn <will.senn@gmail.com>
2026-03-03 22:54:11 -0600

    added some unit tests

---
+++ engine/runner/prelude_help_test.go
@@ -0,0 +1,31 @@
+package runner
+
+import (
+	"testing"
+
+	"aiki/engine/runtime/hal/substrate"
+	"aiki/engine/semantics/value"
+	"aiki/engine/syntax"
+	"aiki/engine/syntax/grammar"
+)
+
+// This is a focused unit test for the same invariant that smoke exercises:
+// prelude.help must fully cover the functions defined in prelude.ai.
+func TestPreludeHelpRegistryComplete(t *testing.T) {
+	g, err := grammar.Load("grammar.ebnfx", syntax.EbnfxSource, "grammar.help", syntax.HelpSource)
+	if err != nil {
+		t.Fatalf("loading grammar: %v", err)
+	}
+
+	// Make sure help registry init does not error
+	if err := initHelpRegistry(); err != nil {
+		t.Fatalf("initHelpRegistry: %v", err)
+	}
+
+	// Create a runtime and prelude env and load the prelude.
+	rt := substrate.NewGoRuntime()
+	preludeEnv := value.NewEnvWithScope(value.ScopePrelude)
+	if err := loadPrelude(g, rt, preludeEnv); err != nil {
+		t.Fatalf("loadPrelude: %v", err)
+	}
+}
